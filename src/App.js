93583860665705def76b470e66489df942371697
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-08-31 13:52:23 -0700

    created search bar and pulled in logs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+import React, { Fragment, useEffect} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -10,9 +12,12 @@ function App() {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
