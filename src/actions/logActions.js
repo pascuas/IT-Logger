aab5e8835532878e59989f045cfa87f1d9e3d599
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-09-09 10:36:58 -0700

    bringing in setCurrent in order to edit which log we want to edit

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG} from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, UPDATE_LOG, SET_CURRENT, CLEAR_CURRENT } from './types';
 
 // export const getLogs = () => {
 //     return async (dispatch) => {
@@ -85,6 +85,49 @@ export const deleteLog = (id) => async dispatch => {
         })
     }
 };
+
+// Update log from server
+export const updateLog = log => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'appliacation/json'
+            }
+        });
+
+        const data = await res.json();
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });
+        
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+};
+
+// Set current log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+// Clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT,
+    }
+}
 
 // Set loading to true
 export const setLoading = () => {
