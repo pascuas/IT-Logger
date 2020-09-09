9c5e5e6ac1af3ec20104347dd223a11e9624e50c
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-09-09 15:21:38 -0700

    getTechs action added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -32,7 +32,7 @@ export const getLogs = () => async dispatch => {
         } catch (err) {
             dispatch({
                 type: LOGS_ERROR,
-                payload: err.response.data
+                payload: err.response.statusText
             })
         }
 };
@@ -59,7 +59,7 @@ export const addLog = (log) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         })
     }
 };
@@ -81,7 +81,7 @@ export const deleteLog = (id) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         })
     }
 };
@@ -109,7 +109,7 @@ export const updateLog = log => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         })
     }
 };
@@ -130,7 +130,7 @@ export const searchLogs = (text) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         })
     }
 };
